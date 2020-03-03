4df10a283f3ed21d1ccc96e808f0dfcd219fdcea
Nevin <nevinsuri@hotmail.com>
2020-03-04 04:19:57 +1100

    do more of rtt_dataset

--- tensorflow_datasets/text/rtt_news_dataset.py
+++ tensorflow_datasets/text/rtt_news_dataset.py
@@ -4,7 +4,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os, sys
+
+root_path = os.path.abspath(__file__)
+while os.path.basename(root_path) != "tensorflow_datasets":
+    print(root_path)
+    root_path = os.path.dirname(root_path)
+root_path = os.path.dirname(root_path)
+print(root_path)
+sys.path.append(root_path)
+
 import tensorflow_datasets.public_api as tfds
+import tensorflow as tf
+
 
 # TODO(rtt_news_dataset): BibTeX citation
 _CITATION = """
@@ -16,10 +28,12 @@ _DESCRIPTION = """
 
 
 class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
-  """TODO(rtt_news_dataset): Short description of my dataset."""
+  """Financial news from https://www.rttnews.com/. Dated 2015-02-19 to 2020-03-02 ."""
 
   # TODO(rtt_news_dataset): Set up version.
   VERSION = tfds.core.Version('0.1.0')
+
+  MANUAL_DOWNLOAD_INSTRUCTIONS = "lmao"
 
   def _info(self):
     # TODO(rtt_news_dataset): Specifies the tfds.core.DatasetInfo object
@@ -30,13 +44,15 @@ class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
         # tfds.features.FeatureConnectors
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
+            "article": tfds.features.Text(),
+            "label": tfds.features.Tensor(shape=(), dtype=tf.float32),
         }),
         # If there's a common (input, target) tuple from the features,
         # specify them here. They'll be used if as_supervised=True in
         # builder.as_dataset.
-        supervised_keys=(),
+        supervised_keys=("article", "label"),
         # Homepage of the dataset for documentation
-        homepage='https://dataset-homepage/',
+        homepage='https://www.rttnews.com/',
         citation=_CITATION,
     )
 
@@ -45,16 +61,27 @@ class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
     # TODO(rtt_news_dataset): Downloads the data and defines the splits
     # dl_manager is a tfds.download.DownloadManager that can be used to
     # download and extract URLs
+    data_path = dl_manager.manual_dir
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
             # These kwargs will be passed to _generate_examples
-            gen_kwargs={},
+            gen_kwargs={
+                "article_text_path": data_path
+            },
         ),
     ]
 
-  def _generate_examples(self):
+  def _generate_examples(self, article_text_path):
     """Yields examples."""
     # TODO(rtt_news_dataset): Yields (key, example) tuples from the dataset
-    yield 'key', {}
+    for article_file in tf.io.gfile.listdir(article_text_path):
+        file_path = os.path.join(article_text_path, article_file)
+        file_ptr = tf.io.gfile.GFile(file_path)
+        label = file_ptr.readline()
+        yield article_file, {
+            "article": file_ptr.read(),
+            "label": label
+        }
+
 
--- tensorflow_datasets/text/rtt_news_dataset_test.py
+++ tensorflow_datasets/text/rtt_news_dataset_test.py
@@ -4,8 +4,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os, sys
+
+root_path = os.path.abspath(__file__)
+while os.path.basename(root_path) != "tensorflow_datasets":
+    print(root_path)
+    root_path = os.path.dirname(root_path)
+root_path = os.path.dirname(root_path)
+print(root_path)
+sys.path.append(root_path)
+
 from tensorflow_datasets import testing
 from tensorflow_datasets.text import rtt_news_dataset
+import tensorflow as tf
 
 
 class RttNewsDatasetTest(testing.DatasetBuilderTestCase):
@@ -21,6 +32,12 @@ class RttNewsDatasetTest(testing.DatasetBuilderTestCase):
   # then the tests needs to provide the fake output paths relative to the
   # fake data directory
   # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
+  DL_EXTRACT_RESULT = {
+    r'2015-02-19_Caution%20Prevails%20As%20Greek%20Crisis%20Continues.txt': r'2015-02-19_Caution%20Prevails%20As%20Greek%20Crisis%20Continues.txt',
+    r'2015-02-19_Stocks%20May%20Continue%20To%20Experience%20Choppy%20Trading%20-%20U.S.%20Commentary.txt': r'2015-02-19_Stocks%20May%20Continue%20To%20Experience%20Choppy%20Trading%20-%20U.S.%20Commentary.txt',
+    r'2015-02-19_Wall%20Street%20Seen%20Lower%20Amid%20Continuing%20Uncertainty.txt': r'2015-02-19_Wall%20Street%20Seen%20Lower%20Amid%20Continuing%20Uncertainty.txt',
+    r'2015-02-20_French%20Market%20Slides%20Amid%20Greek%20Concerns.txt': r'2015-02-20_French%20Market%20Slides%20Amid%20Greek%20Concerns.txt',
+  }
 
 
 if __name__ == "__main__":
