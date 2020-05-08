da3a1128e92d87a27abb098b1734da46403c2298
Nevin <nevinsuri@hotmail.com>
2020-05-09 02:47:56 +1000

    add date of news article to data set

--- tensorflow_datasets/text/rtt_news_dataset.py
+++ tensorflow_datasets/text/rtt_news_dataset.py
@@ -11,10 +11,8 @@ import math
 
 root_path = os.path.abspath(__file__)
 while os.path.basename(root_path) != "tensorflow_datasets":
-    print(root_path)
     root_path = os.path.dirname(root_path)
 root_path = os.path.dirname(root_path)
-print(root_path)
 sys.path.append(root_path)
 
 import tensorflow_datasets.public_api as tfds
@@ -34,7 +32,7 @@ class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
   """Financial news from https://www.rttnews.com/. Dated 2015-02-19 to 2020-03-02 ."""
 
   # TODO(rtt_news_dataset): Set up version.
-  VERSION = tfds.core.Version('0.1.0')
+  VERSION = tfds.core.Version('0.2.0')
 
   MANUAL_DOWNLOAD_INSTRUCTIONS = "lmao"
 
@@ -48,6 +46,7 @@ class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
             "article": tfds.features.Text(),
+            "date": tfds.features.Tensor(shape=(), dtype=tf.int64),
             "label": tfds.features.Tensor(shape=(), dtype=tf.float32),
         }),
         # If there's a common (input, target) tuple from the features,
@@ -68,18 +67,29 @@ class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
     price_df = pd.read_csv(os.path.join(data_path, 'market_data', 'GSPC.csv'))
     price_df['Date'] = pd.to_datetime(price_df['Date'])
     price_df.set_index('Date', inplace=True)
+    date_split = datetime.date(2019, 2, 1)
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
-            # These kwargs will be passed to _generate_examples
             gen_kwargs={
                 "article_text_path": data_path,
-                "price_df": price_df
+                "price_df": price_df.loc[:date_split],
+                "date_split": date_split,
+                "split_before": True
             },
         ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs={
+                "article_text_path": data_path,
+                "price_df": price_df.loc[date_split + datetime.timedelta(1):],
+                "date_split": date_split,
+                "split_before": False
+            },
+        )
     ]
 
-  def _generate_examples(self, article_text_path, price_df):
+  def _generate_examples(self, article_text_path, price_df, date_split, split_before):
     """Yields examples."""
     # TODO(rtt_news_dataset): Yields (key, example) tuples from the dataset
     for article_file in tf.io.gfile.listdir(article_text_path):
@@ -87,15 +97,28 @@ class RttNewsDataset(tfds.core.GeneratorBasedBuilder):
         if tf.io.gfile.isdir(file_path): continue
         file_ptr = tf.io.gfile.GFile(file_path)
 
-        desired_date = pd.Timestamp(datetime.date.fromisoformat(article_file[:10]))
         label = 0
-        for j in range(len(price_df)):
-            if price_df.index[j] >= desired_date:
-                row = price_df.iloc[j]
-                label = math.log(row['Close'] / row['Open'])
+
+        article_date = datetime.date.fromisoformat(article_file[:10])
+        desired_date = article_date + datetime.timedelta(1)
+
+        if ((split_before and desired_date > date_split) or
+            (not split_before and desired_date < split_before)):
+            continue
+
+        desired_date_pdt = pd.Timestamp(desired_date)
+        while desired_date_pdt not in price_df.index and desired_date < datetime.date(2100, 1, 1):
+            desired_date += datetime.timedelta(days=1)
+            desired_date_pdt = pd.Timestamp(desired_date)
+
+        if desired_date_pdt in price_df.index:
+            row = price_df.loc[desired_date_pdt]
+            label = math.log(row['Close'] / row['Open'])
+
         yield article_file, {
             "article": file_ptr.read(),
+            "date": int(datetime.datetime.combine(
+                article_date, datetime.datetime.min.time()).timestamp()),
             "label": label
         }
-
-
+        file_ptr.close()
